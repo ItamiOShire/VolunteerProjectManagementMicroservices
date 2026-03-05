49ee44f1abbdadf666322a6aed3527fea33364ed
Adam <a.niewczas01@gmail.com>
2026-03-05 20:47:25 +0100

    feat-authserver: Custom Authentication configuration featuring logging users in by email

---
+++ AuthenticationServer/src/main/java/com/vpm/authenticationserver/config/AuthenticationConfig.java
@@ -0,0 +1,44 @@
+package com.vpm.authenticationserver.config;
+
+
+import com.vpm.authenticationserver.service.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class AuthenticationConfig {
+
+    private final CustomUserDetailsService userDetailsService;
+
+    @Autowired
+    public AuthenticationConfig(CustomUserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationProvider authenticationProvider) {
+        return new ProviderManager(authenticationProvider);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
---
+++ AuthenticationServer/src/main/java/com/vpm/authenticationserver/service/CustomUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.vpm.authenticationserver.service;
+
+import com.vpm.authenticationserver.entity.Users;
+import com.vpm.authenticationserver.repository.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UsersRepository usersRepository;
+
+    @Autowired
+    public CustomUserDetailsService(UsersRepository usersRepository) {
+        this.usersRepository = usersRepository;
+    }
+
+    @Override
+    public Users loadUserByUsername(String email) throws UsernameNotFoundException {
+        return usersRepository.findById(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+    }
+
+}
