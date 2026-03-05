a903f63003f5759e0b3186cedab18f1555ca58ba
Adam <a.niewczas01@gmail.com>
2026-03-05 20:47:41 +0100

    feat-authserver: users repository

---
+++ AuthenticationServer/src/main/java/com/vpm/authenticationserver/repository/UsersRepository.java
@@ -0,0 +1,9 @@
+package com.vpm.authenticationserver.repository;
+
+import com.vpm.authenticationserver.entity.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsersRepository extends JpaRepository<Users, String> {
+}
