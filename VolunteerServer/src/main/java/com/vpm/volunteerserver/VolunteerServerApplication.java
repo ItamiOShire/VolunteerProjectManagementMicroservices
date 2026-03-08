fb3e32f56befd061eb104c7b1a998020a6dfc2bb
Adam <a.niewczas01@gmail.com>
2026-03-08 13:21:39 +0100

    init-volunteerserver: init commit

---
+++ VolunteerServer/src/main/java/com/vpm/volunteerserver/VolunteerServerApplication.java
@@ -0,0 +1,13 @@
+package com.vpm.volunteerserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolunteerServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VolunteerServerApplication.class, args);
+    }
+
+}
