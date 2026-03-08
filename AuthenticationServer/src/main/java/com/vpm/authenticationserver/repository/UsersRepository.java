4f555ce1d661666390b6cd598f7fd0f45fdef467
Adam <a.niewczas01@gmail.com>
2026-03-08 13:04:47 +0100

    chore: imports cleanup

--- AuthenticationServer/src/main/java/com/vpm/authenticationserver/repository/UsersRepository.java
+++ AuthenticationServer/src/main/java/com/vpm/authenticationserver/repository/UsersRepository.java
@@ -4,7 +4,6 @@ import com.vpm.authenticationserver.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.lang.ScopedValue;
 import java.util.Optional;
 
 @Repository
