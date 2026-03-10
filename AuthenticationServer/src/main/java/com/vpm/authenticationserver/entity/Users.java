20e91489c17b44f906170311c94c71257d51323e
Adam <a.niewczas01@gmail.com>
2026-03-10 16:00:10 +0100

    feat-authserver: builder pattern for users entiity

--- AuthenticationServer/src/main/java/com/vpm/authenticationserver/entity/Users.java
+++ AuthenticationServer/src/main/java/com/vpm/authenticationserver/entity/Users.java
@@ -1,10 +1,7 @@
 package com.vpm.authenticationserver.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -17,6 +14,7 @@ import java.util.Set;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class Users implements UserDetails {
 
     @Id
