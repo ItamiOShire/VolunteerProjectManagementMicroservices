ca777f7a40b897244eebe318e88cd80ac25ce8ee
Adam <a.niewczas01@gmail.com>
2026-03-10 13:05:45 +0100

    chore-volunteerserver: update imports with common dtos

--- VolunteerServer/src/main/java/com/vpm/volunteerserver/api/internal/AuthClient.java
+++ VolunteerServer/src/main/java/com/vpm/volunteerserver/api/internal/AuthClient.java
@@ -1,11 +1,11 @@
 package com.vpm.volunteerserver.api.internal;
 
-import com.vpm.volunteerserver.dto.request.AuthRequest;
-import com.vpm.volunteerserver.dto.response.AuthResponse;
+import com.vpm.common.dto.request.AuthRequest;
+import com.vpm.common.dto.response.AuthResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@FeignClient(name = "auth-service", url = "${services.auth-service.url")
+@FeignClient(name = "auth-service", url = "${services.auth-service.url}")
 public interface AuthClient {
 
     @PostMapping("/user")
