23a4ecca068a964d208669497ed38df273716e0c
Adam <a.niewczas01@gmail.com>
2026-03-10 15:30:47 +0100

    feat-volunteerserver: builder pattern for volunteer entity

--- VolunteerServer/src/main/java/com/vpm/volunteerserver/entity/Volunteer.java
+++ VolunteerServer/src/main/java/com/vpm/volunteerserver/entity/Volunteer.java
@@ -2,10 +2,7 @@ package com.vpm.volunteerserver.entity;
 
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDate;
 
@@ -16,6 +13,7 @@ import java.time.LocalDate;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Volunteer {
 
     @Id
