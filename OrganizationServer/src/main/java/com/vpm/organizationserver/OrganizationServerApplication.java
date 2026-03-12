46b885078c8c2e6ea146a9f057eae5830c217f7f
Adam <a.niewczas01@gmail.com>
2026-03-12 15:26:13 +0100

    init organization server module

---
+++ OrganizationServer/src/main/java/com/vpm/organizationserver/OrganizationServerApplication.java
@@ -0,0 +1,13 @@
+package com.vpm.organizationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganizationServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrganizationServerApplication.class, args);
+    }
+
+}
