e806015638a678eac58c6a0085eb98b6cc0cb2a5
Adam <a.niewczas01@gmail.com>
2026-03-12 15:54:50 +0100

    feat-organizationserver: global exception handling

---
+++ OrganizationServer/src/main/java/com/vpm/organizationserver/exception/ApiException.java
@@ -0,0 +1,22 @@
+package com.vpm.organizationserver.exception;
+
+import com.vpm.common.error.ErrorCode;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class ApiException extends RuntimeException {
+
+    private final HttpStatus status;
+    private final ErrorCode errorCode;
+
+    public ApiException(
+            String message,
+            HttpStatus status,
+            ErrorCode errorCode
+    ) {
+        super(message);
+        this.status = status;
+        this.errorCode = errorCode;
+    }
+}
---
+++ OrganizationServer/src/main/java/com/vpm/organizationserver/exception/GlobalExceptionHandler.java
@@ -0,0 +1,47 @@
+package com.vpm.organizationserver.exception;
+
+import com.vpm.common.error.ErrorCode;
+import com.vpm.common.error.ErrorResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ApiException.class)
+    public ResponseEntity<ErrorResponse> handleApiException(
+            ApiException e,
+            WebRequest request
+    ) {
+        ErrorResponse error = new ErrorResponse(
+                e.getStatus().value(),
+                e.getErrorCode(),
+                e.getMessage(),
+                request.getContextPath()
+        );
+
+        return ResponseEntity
+                .status(e.getStatus())
+                .body(error);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(
+            Exception e,
+            WebRequest request
+    ) {
+        ErrorResponse error = new ErrorResponse(
+                500,
+                ErrorCode.INTERNAL_SERVER_ERROR,
+                "An unexpected error occurred",
+                request.getContextPath()
+        );
+
+        return ResponseEntity
+                .status(500)
+                .body(error);
+    }
+
+}
