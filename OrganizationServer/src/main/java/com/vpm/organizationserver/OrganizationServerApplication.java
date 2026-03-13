38a251eb462adf05921d8310b8032ef387fa4155
Adam <a.niewczas01@gmail.com>
2026-03-13 16:48:50 +0100

    feat-organizationserver: enable feign client in app

--- OrganizationServer/src/main/java/com/vpm/organizationserver/OrganizationServerApplication.java
+++ OrganizationServer/src/main/java/com/vpm/organizationserver/OrganizationServerApplication.java
@@ -2,8 +2,10 @@ package com.vpm.organizationserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class OrganizationServerApplication {
 
     public static void main(String[] args) {
--- OrganizationServer/src/main/java/com/vpm/organizationserver/api/internal/AuthClient.java
+++ OrganizationServer/src/main/java/com/vpm/organizationserver/api/internal/AuthClient.java
@@ -5,9 +5,10 @@ import com.vpm.common.dto.response.AuthRegistrationResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@FeignClient(name = "auth-server", url = "${services.auth-service.url}")
+@FeignClient(name = "auth-service", url = "${services.auth-service.url}")
 public interface AuthClient {
 
     @PostMapping("/")
     AuthRegistrationResponse registerOrganizationInAuthServer(AuthRegistrationRequest request);
+
 }
