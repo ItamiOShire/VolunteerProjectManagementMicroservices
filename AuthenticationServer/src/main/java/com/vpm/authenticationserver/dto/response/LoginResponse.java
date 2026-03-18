657e882a938806883b8712354a2e8f4501decd45
Adam <a.niewczas01@gmail.com>
2026-03-18 13:30:24 +0100

    chore-authserver: add missing commit with login response

---
+++ AuthenticationServer/src/main/java/com/vpm/authenticationserver/dto/response/LoginResponse.java
@@ -0,0 +1,24 @@
+package com.vpm.authenticationserver.dto.response;
+
+import com.vpm.authenticationserver.entity.Users;
+
+public record LoginResponse(
+        String accessToken,
+        String refreshToken,
+        String role,
+        long id) {
+
+    public LoginResponse(
+            String accessToken,
+            String refreshToken,
+            Users user
+    ) {
+        this(
+                accessToken,
+                refreshToken,
+                user.getRole(),
+                user.getId()
+        );
+    }
+
+}
