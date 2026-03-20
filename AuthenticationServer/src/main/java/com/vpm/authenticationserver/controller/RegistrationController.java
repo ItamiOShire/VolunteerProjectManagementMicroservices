e8032e48c6bc3fd4daa6f0e8736eca6b56db4007
Adam <a.niewczas01@gmail.com>
2026-03-20 17:02:24 +0100

    feat-authserver: move headers validation logic into spring RequestFilter component

--- AuthenticationServer/src/main/java/com/vpm/authenticationserver/controller/RegistrationController.java
+++ AuthenticationServer/src/main/java/com/vpm/authenticationserver/controller/RegistrationController.java
@@ -1,17 +1,10 @@
 package com.vpm.authenticationserver.controller;
 
-import com.vpm.authenticationserver.exception.request.InvalidServiceNameHeaderException;
-import com.vpm.authenticationserver.exception.request.MissingRequiredHeaderException;
 import com.vpm.authenticationserver.service.RegistrationService;
 import com.vpm.common.dto.request.AuthRegistrationRequest;
 import com.vpm.common.dto.response.AuthRegistrationResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 @RestController
 @RequestMapping("/api/internal/registration")
@@ -19,35 +12,9 @@ public class RegistrationController {
 
     private final RegistrationService registrationService;
 
-    /*
-     * services allowed for internal API communication with Authentication Server. This is a basic validation mechanism to ensure that only authorized services can access the registration endpoint.
-     */
-    private final Set<String> allowedServices = new HashSet<>(
-            Set.of(
-                    "volunteer-service",
-                    "organization-service"
-            )
-    );
-
     @Autowired
     public RegistrationController(RegistrationService registrationService) {
         this.registrationService = registrationService;
-    }
-
-    private void validateInternalRequest(String internalRequestHeader, String serviceNameHeader) {
-
-        //TODO: improve validation so headers name are not hardcoded (request headers are handled automatically by advisor!!!)
-
-        if (internalRequestHeader == null || serviceNameHeader == null) {
-            List<String> headers = new ArrayList<>();
-            if (internalRequestHeader == null) headers.add("X-INTERNAL-REQUEST");
-            if (serviceNameHeader == null) headers.add("X-SERVICE-NAME");
-            throw new MissingRequiredHeaderException(headers);
-        }
-
-        if (!allowedServices.contains(serviceNameHeader.toLowerCase())) {
-            throw new InvalidServiceNameHeaderException(serviceNameHeader);
-        }
     }
 
     /*
@@ -62,11 +29,6 @@ public class RegistrationController {
             @RequestHeader ( value = "X-SERVICE-NAME") String serviceNameHeader,
             @RequestBody AuthRegistrationRequest request
     ) {
-
-        validateInternalRequest(
-                internalRequestHeader,
-                serviceNameHeader
-        );
 
         return registrationService.registerUserInAuthService(request);
 
---
+++ AuthenticationServer/src/main/java/com/vpm/authenticationserver/security/RequestFilter.java
@@ -0,0 +1,52 @@
+package com.vpm.authenticationserver.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.jspecify.annotations.NullMarked;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+@Component
+public class RequestFilter extends OncePerRequestFilter {
+
+    /*
+     * services allowed for internal API communication with Authentication Server. This is a basic validation mechanism to ensure that only authorized services can access the registration endpoint.
+     */
+    private final Set<String> allowedServices = new HashSet<>(
+            Set.of(
+                    "volunteer-service",
+                    "organization-service"
+            )
+    );
+
+    @Override
+    @NullMarked
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        if (request.getRequestURI().startsWith("/api/internal")) {
+            String internalRequest = request.getHeader("X-INTERNAL-REQUEST");
+            String serviceName = request.getHeader("X-SERVICE-NAME");
+
+            if (internalRequest == null || !internalRequest.equals("true")) {
+                response.sendError(HttpServletResponse.SC_FORBIDDEN, "Invalid or missing X-INTERNAL-REQUEST header");
+
+                return;
+            }
+
+            if (serviceName == null || !allowedServices.contains(serviceName)) {
+                response.sendError(HttpServletResponse.SC_FORBIDDEN, "Invalid or missing X-SERVICE-NAME header");
+                return;
+            }
+        }
+
+        filterChain.doFilter(request, response);
+
+    }
+
+}
