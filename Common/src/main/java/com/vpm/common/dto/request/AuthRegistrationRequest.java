a3e838c9b53eb7ee7a72638dda6b5b4fa66aca30
Adam <a.niewczas01@gmail.com>
2026-03-20 16:58:52 +0100

    ref-common: use lombok to remove boilerplate code

--- Common/src/main/java/com/vpm/common/dto/request/AuthRegistrationRequest.java
+++ Common/src/main/java/com/vpm/common/dto/request/AuthRegistrationRequest.java
@@ -1,38 +1,15 @@
 package com.vpm.common.dto.request;
 
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class AuthRegistrationRequest {
 
     private String email;
     private String password;
     private String role;
 
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public AuthRegistrationRequest(String email, String password, String role) {
-        this.email = email;
-        this.password = password;
-        this.role = role;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
 }
--- Common/src/main/java/com/vpm/common/dto/response/AuthRegistrationResponse.java
+++ Common/src/main/java/com/vpm/common/dto/response/AuthRegistrationResponse.java
@@ -1,18 +1,12 @@
 package com.vpm.common.dto.response;
 
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class AuthRegistrationResponse {
 
     private long userId;
-
-    public AuthRegistrationResponse(long userId) {
-        this.userId = userId;
-    }
-
-    public long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(long userId) {
-        this.userId = userId;
-    }
 }
