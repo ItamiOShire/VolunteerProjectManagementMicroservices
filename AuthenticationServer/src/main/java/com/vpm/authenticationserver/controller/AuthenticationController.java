4c34744079a39b360ae4c1d411842a833a04f1ea
Adam <a.niewczas01@gmail.com>
2026-03-20 17:00:30 +0100

    feat-authserver: spring validation in login logic

--- AuthenticationServer/src/main/java/com/vpm/authenticationserver/controller/AuthenticationController.java
+++ AuthenticationServer/src/main/java/com/vpm/authenticationserver/controller/AuthenticationController.java
@@ -5,10 +5,8 @@ import com.vpm.authenticationserver.dto.request.LoginRequest;
 import com.vpm.authenticationserver.service.AuthenticationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -21,9 +19,9 @@ public class AuthenticationController {
         this.authenticationService = authenticationService;
     }
 
-    @GetMapping("/login")
+    @PostMapping("/login")
     public ResponseEntity<?> login(
-            @RequestBody LoginRequest loginRequest
+            @Validated @RequestBody LoginRequest loginRequest
             ) {
         return ResponseEntity
                 .ok(authenticationService.login(loginRequest));
--- AuthenticationServer/src/main/java/com/vpm/authenticationserver/dto/request/LoginRequest.java
+++ AuthenticationServer/src/main/java/com/vpm/authenticationserver/dto/request/LoginRequest.java
@@ -1,4 +1,13 @@
 package com.vpm.authenticationserver.dto.request;
 
-public record LoginRequest(String email, String password) {
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+public record LoginRequest(
+        @NotNull(message = "email field cannot be null")
+        @NotEmpty(message = "email field cannot be empty") String email,
+
+        @NotEmpty(message = "password field cannot be null")
+        @NotNull(message = "password field cannot be empty") String password) {
 }
