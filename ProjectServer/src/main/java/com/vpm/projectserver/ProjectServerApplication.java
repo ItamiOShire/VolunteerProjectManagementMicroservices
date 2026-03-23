27e89281f698fb5245c51538f50e20fe816ad501
Adam <a.niewczas01@gmail.com>
2026-03-23 11:10:57 +0100

    init-projectservice: init project server

---
+++ ProjectServer/src/main/java/com/vpm/projectserver/ProjectServerApplication.java
@@ -0,0 +1,17 @@
+package com.vpm.projectserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+@EnableDiscoveryClient
+public class ProjectServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectServerApplication.class, args);
+    }
+
+}
