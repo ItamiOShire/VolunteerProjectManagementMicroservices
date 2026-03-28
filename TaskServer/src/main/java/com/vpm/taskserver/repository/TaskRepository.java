a00a93f47ce82776c137034904d8c106817688fb
Adam <a.niewczas01@gmail.com>
2026-03-28 10:55:32 +0100

    feat-taskserver: entities repositories

---
+++ TaskServer/src/main/java/com/vpm/taskserver/repository/TaskRepository.java
@@ -0,0 +1,43 @@
+package com.vpm.taskserver.repository;
+
+import com.vpm.taskserver.entity.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    
+    List<Task> getTasksByProjectId(Long projectId);
+    
+    @Query("""
+    SELECT t FROM Task t
+    JOIN FETCH t.volunteerTasks vt
+    ON vt.id.volunteerUserId = :volunteerId
+""")
+    List<Task> getTasksByVolunteerId(@Param("volunteerId")  Long volunteerId);
+
+    @Query("""
+    SELECT t FROM Task t
+    JOIN FETCH t.volunteerTasks vt
+    WHERE t.projectId = :projectId AND vt.id.volunteerUserId = :volunteerId
+""")
+    List<Task> getTasksInProjectByVolunteerId(
+            @Param("projectId") Long projectId,
+            @Param("volunteerId") Long volunteerId
+    );
+
+    @Query("""
+    SELECT t FROM Task t
+    JOIN FETCH t.taskSuggestions ts
+    WHERE t.projectId = :projectId AND ts.id.volunteerUserId = :volunteerId
+""")
+    List<Task> getTaskSuggestionsInProjectByVolunteerId(
+            @Param("projectId") long projectId,
+            @Param("volunteerId") long volunteerId
+    );
+
+}
