afb95a215c0d61a2485631d6ba60b58f1b87dcf3
Adam <a.niewczas01@gmail.com>
2026-03-28 10:57:46 +0100

    feat-taskserver: request header filter

---
+++ TaskServer/src/main/java/com/vpm/taskserver/security/RequestFilter.java
@@ -0,0 +1,50 @@
+package com.vpm.taskserver.security;
+
+import com.vpm.taskserver.exception.request.MissingRequiredHeaderException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.jspecify.annotations.NullMarked;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Component
+public class RequestFilter extends OncePerRequestFilter {
+
+    /*
+     * Set of required headers - those headers are signature of authorized request
+     * Gateway adds those headers if user is authorized
+     */
+    private final Set<String> requiredHeaders = new HashSet<>(
+            Set.of(
+                    "X-User-Id",
+                    "X-User-Role"
+            )
+    );
+
+    @Override
+    @NullMarked
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws MissingRequiredHeaderException {
+
+        for (String header : requiredHeaders) {
+
+            String headerValue = request.getHeader(header);
+
+            if (headerValue == null || headerValue.isEmpty()) {
+
+                throw new MissingRequiredHeaderException(header);
+
+            }
+
+        }
+
+    }
+
+}
