b4f0544da0b3d4e151f1e158983cbac34d434eea
Adam <a.niewczas01@gmail.com>
2026-03-30 19:15:38 +0200

    feat-organizationserver: implementation of filtering headers of requests

---
+++ OrganizationServer/src/main/java/com/vpm/organizationserver/security/RequestFilter.java
@@ -0,0 +1,57 @@
+package com.vpm.organizationserver.security;
+
+import com.vpm.organizationserver.exception.request.MissingRequiredHeaderException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.jspecify.annotations.NullMarked;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+@Component
+public class RequestFilter extends OncePerRequestFilter {
+
+    /*
+     * Set of required headers - those headers are signature of authorized request
+     * Gateway adds those headers if user is authorized
+     */
+    private final Set<String> requiredHeaders = new HashSet<>(
+            Set.of(
+                    "X-User-Id",
+                    "X-User-Role"
+            )
+    );
+
+    @Override
+    @NullMarked
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws MissingRequiredHeaderException, IOException, ServletException {
+
+        for (String header : requiredHeaders) {
+
+            String headerValue = request.getHeader(header);
+
+            if (headerValue == null || headerValue.isEmpty()) {
+
+                response.sendError(
+                        HttpServletResponse.SC_BAD_REQUEST,
+                        String.format("Missing required header: %s", header)
+                );
+
+            }
+
+        }
+
+        filterChain.doFilter(request, response);
+
+    }
+
+}
