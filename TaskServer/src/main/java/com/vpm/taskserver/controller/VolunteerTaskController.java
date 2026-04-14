229405e66d4945539665a8f405f00440ec36dc3b
Adam <a.niewczas01@gmail.com>
2026-04-14 13:20:33 +0200

    chore-taskserver: code cleanup

--- TaskServer/src/main/java/com/vpm/taskserver/controller/VolunteerTaskController.java
+++ TaskServer/src/main/java/com/vpm/taskserver/controller/VolunteerTaskController.java
@@ -1,7 +1,6 @@
 package com.vpm.taskserver.controller;
 
 
-import com.vpm.taskserver.entity.Task;
 import com.vpm.taskserver.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -10,7 +9,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/volunteers")
