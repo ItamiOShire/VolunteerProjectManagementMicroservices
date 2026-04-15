11d5dff801ea1eca8f58cf909494b34a5711baf0
Adam <a.niewczas01@gmail.com>
2026-04-15 19:31:12 +0200

    feat-organizationserver: handling spring boot MVC and data validation exceptions

--- OrganizationServer/src/main/java/com/vpm/organizationserver/exception/GlobalExceptionHandler.java
+++ OrganizationServer/src/main/java/com/vpm/organizationserver/exception/GlobalExceptionHandler.java
@@ -4,8 +4,11 @@ import com.vpm.common.error.ErrorCode;
 import com.vpm.common.error.ErrorResponse;
 import com.vpm.organizationserver.exception.request.MissingRequiredHeaderException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageConversionException;
+import org.springframework.validation.BindException;
 import org.springframework.web.bind.MissingRequestHeaderException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -14,6 +17,13 @@ import org.springframework.web.context.request.WebRequest;
 @RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
+
+    /**
+     * Exceptions thrown in business layer and logged there
+     * @param e custom exception api
+     * @param request client request
+     * @return ResponseEntity with http status and body of ErrorResponse
+     */
 
     @ExceptionHandler(ApiException.class)
     public ResponseEntity<ErrorResponse> handleApiException(
@@ -32,7 +42,45 @@ public class GlobalExceptionHandler {
                 .body(error);
     }
 
-    @ExceptionHandler(MissingRequiredHeaderException.class)
+    @ExceptionHandler(HttpMessageConversionException.class)
+    public ResponseEntity<ErrorResponse> handleHttpMessageConversionException(
+            HttpMessageConversionException e,
+            WebRequest request
+    ) {
+        ErrorResponse error = new ErrorResponse(
+                400,
+                ErrorCode.BAD_REQUEST,
+                "Error during body conversion: Exception - " + e.getClass().getName() + ", message - " + e.getMessage(),
+                request.getContextPath()
+        );
+
+        log.error("Error during body conversion: {}", e.getMessage());
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(error);
+    }
+
+    @ExceptionHandler(BindException.class)
+    public ResponseEntity<ErrorResponse> handleBindException(
+            BindException e,
+            WebRequest request
+    ) {
+        ErrorResponse error = new ErrorResponse(
+                400,
+                ErrorCode.BAD_REQUEST,
+                "Validation error: " + e.getMessage(),
+                request.getContextPath()
+        );
+
+        log.error("Error during fields validation: {}", e.getAllErrors().stream().map(DefaultMessageSourceResolvable::getDefaultMessage).toList());
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(error);
+    }
+
+    @ExceptionHandler(MissingRequestHeaderException.class)
     public ResponseEntity<ErrorResponse> handleMissingRequestHeaderException(
             MissingRequestHeaderException e,
             WebRequest request
@@ -59,9 +107,11 @@ public class GlobalExceptionHandler {
         ErrorResponse error = new ErrorResponse(
                 500,
                 ErrorCode.INTERNAL_SERVER_ERROR,
-                "An unexpected error occurred",
+                "An unexpected error occurred: " + e.getClass().getName() + " - " + e.getMessage() ,
                 request.getContextPath()
         );
+
+        log.error("An unexpected error occurred: {} - {}", e.getClass().getName(), e.getMessage());
 
         return ResponseEntity
                 .status(500)
