34a048df6d3d96ed8e3ef34cfaccd704da2cf05c
Adam <a.niewczas01@gmail.com>
2026-04-15 19:52:38 +0200

    feat-projectserver: developer defined repository queries for avoiding n+1 hibernate queries problem and too early session closeups

--- ProjectServer/src/main/java/com/vpm/projectserver/repository/ProjectRepository.java
+++ ProjectServer/src/main/java/com/vpm/projectserver/repository/ProjectRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ProjectRepository extends JpaRepository<Project, Long> {
@@ -14,10 +15,33 @@ public interface ProjectRepository extends JpaRepository<Project, Long> {
     @Query("""
     SELECT p FROM Project p
     JOIN FETCH p.volunteers v
+    JOIN FETCH p.tags
     WHERE v.volunteerUserId = :volunteerId
 """)
     List<Project> getProjectsByVolunteerId(@Param("volunteerId") Long id);
 
-    List<Project> getProjectsByOrganizationUserId(Long id);
+    /**
+     * Solves N+1 queries problem - use this on entities where you use relations mapping (n-m, n-1)
+     * @return
+     */
+    @Query("""
+    SELECT p FROM Project p
+    LEFT JOIN FETCH p.tags
+""")
+    List<Project> getAllProjectsWithTags();
+
+    @Query("""
+    SELECT p FROM Project p
+    LEFT JOIN FETCH p.tags
+    WHERE p.id = :projectId
+""")
+    Optional<Project> getProjectById(@Param("projectId")Long id);
+
+    @Query("""
+    SELECT p FROM Project p
+    JOIN FETCH p.tags
+    WHERE p.organizationUserId = :organizationUserId
+""")
+    List<Project> getProjectsByOrganizationUserId(@Param("organizationUserId") Long id);
 
 }
