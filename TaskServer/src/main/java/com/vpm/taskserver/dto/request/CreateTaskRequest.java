fc3ec9a28e869cf4a18da138c66bc16b6acfd35e
Adam <a.niewczas01@gmail.com>
2026-04-15 19:57:37 +0200

    feat-taskserver: data validation for requests

--- TaskServer/src/main/java/com/vpm/taskserver/dto/request/CreateTaskRequest.java
+++ TaskServer/src/main/java/com/vpm/taskserver/dto/request/CreateTaskRequest.java
@@ -1,5 +1,9 @@
 package com.vpm.taskserver.dto.request;
 
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -7,14 +11,21 @@ import java.time.LocalDate;
 @Data
 public class CreateTaskRequest {
 
+    @Positive
     private long projectId;
 
+    @Positive
     private long priorityId;
 
+    @NotBlank(message = "Title must not be blank")
+    @Size(min = 5, max = 50, message = "Title must be between 5 and 50 characters long")
     private String title;
 
+    @NotBlank(message = "Description must not be blank")
     private String description;
 
+    @NotBlank(message = "Deadline must not be blank")
+    @Future(message = "Deadline must be a future date")
     private LocalDate deadline;
 
 }
--- TaskServer/src/main/java/com/vpm/taskserver/dto/request/UpdateTaskRequest.java
+++ TaskServer/src/main/java/com/vpm/taskserver/dto/request/UpdateTaskRequest.java
@@ -1,5 +1,9 @@
 package com.vpm.taskserver.dto.request;
 
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -7,11 +11,17 @@ import java.time.LocalDate;
 @Data
 public class UpdateTaskRequest {
 
+    @NotBlank(message = "Title must not be blank")
+    @Size(min = 5, max = 50, message = "Title must be between 5 and 50 characters long")
     private String title;
 
+    @NotBlank(message = "Description must not be blank")
     private String description;
 
+    @Positive
     private long priorityId;
 
+    @NotBlank(message = "Deadline must not be blank")
+    @Future(message = "Deadline must be a future date")
     private LocalDate deadline;
 }
