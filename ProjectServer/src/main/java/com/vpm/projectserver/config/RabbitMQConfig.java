a91b542074309363c44274c2286586a99e80f943
Adam <a.niewczas01@gmail.com>
2026-05-06 13:05:53 +0200

    fix-projectserver: set message converter for rabbit template for proper serialization

--- ProjectServer/src/main/java/com/vpm/projectserver/config/RabbitMQConfig.java
+++ ProjectServer/src/main/java/com/vpm/projectserver/config/RabbitMQConfig.java
@@ -5,6 +5,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -92,6 +94,7 @@ public class RabbitMQConfig {
             ConnectionFactory connectionFactory
     ) {
         RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(jsonMessageConverter());
         template.setMandatory(true);
 
         template.setConfirmCallback(
@@ -113,6 +116,11 @@ public class RabbitMQConfig {
         );
 
         return template;
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
     }
 
 }
