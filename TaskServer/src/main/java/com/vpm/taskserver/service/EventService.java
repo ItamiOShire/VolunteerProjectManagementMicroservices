4d39999182ecb2f9e637a931f2c8b720dabe4d8d
Adam <a.niewczas01@gmail.com>
2026-05-06 13:07:15 +0200

    fix-taskserver: event service now properly sends message

--- TaskServer/src/main/java/com/vpm/taskserver/service/EventService.java
+++ TaskServer/src/main/java/com/vpm/taskserver/service/EventService.java
@@ -3,10 +3,13 @@ package com.vpm.taskserver.service;
 import com.vpm.taskserver.dto.event.EventType;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.MessagePostProcessor;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
+
+// TODO: Consider making method of EventService asynchronous
 
 @Service
 @Slf4j
@@ -22,28 +25,33 @@ public class EventService {
      * Generic method to send any event to RabbitMQ
      *
      * @param event the event object to send (already mapped from domain object)
-     * @param exchange the RabbitMQ exchange name
+     * @param routingKey the RabbitMQ routing key name
      * @param eventType the type of event (for header)
      * @param <T> the type of event
      */
-    public <T> void sendEvent(T event, String exchange, EventType eventType) {
+    public <T> void sendEvent(T event, String routingKey, String exchange, EventType eventType) {
+
+        String correlationId = UUID.randomUUID().toString();
 
         MessagePostProcessor messagePostProcessor = message -> {
-            message.getMessageProperties().setCorrelationId(UUID.randomUUID().toString());
+            message.getMessageProperties().setCorrelationId(correlationId);
             message.getMessageProperties().setHeader("eventType", eventType);
             return message;
         };
+
+        CorrelationData correlationData = new CorrelationData(correlationId);
 
         log.info("Attempting to send event of type: {}", eventType);
 
         rabbitTemplate.convertAndSend(
                 exchange,
+                routingKey,
                 event,
-                messagePostProcessor
+                messagePostProcessor,
+                correlationData
         );
 
         log.info("Event of type {} successfully sent", eventType);
-
     }
 
 }
