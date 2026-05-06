70b8ac5f45c71eb7926898f391c77405f8ef168c
Adam <a.niewczas01@gmail.com>
2026-05-06 13:11:09 +0200

    feat-volunteerserver: rabbitmq test configuration

---
+++ VolunteerServer/src/test/java/com/vpm/volunteerserver/config/IntegrationTestsRabbitMQConfig.java
@@ -0,0 +1,31 @@
+package com.vpm.volunteerserver.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.rabbitmq.RabbitMQContainer;
+
+@TestConfiguration
+@Slf4j
+public class IntegrationTestsRabbitMQConfig {
+
+    private static final String QUEUE_VOLUNTEER_ASSIGNED = "test.queue.volunteer-project";
+    private static final String DLQ_VOLUNTEER_ASSIGNED = "test.dlq.volunteer-project";
+
+    private static final String EXCHANGE_VOLUNTEER_ASSIGNED = "test.exchange.volunteer-project";
+    private static final String DLX_VOLUNTEER_ASSIGNED = "test.dlx.volunteer-project";
+
+    private static final String ROUTING_KEY_VOLUNTEER_ASSIGNED = "test.routingkey.volunteer-project";
+    private static final String DLQ_ROUTING_KEY_ASSIGNED = "test.dlq.routiongkey.volunteer-project";
+
+    @Bean
+    @ServiceConnection
+    public RabbitMQContainer rabbitMQContainer() {
+        return new RabbitMQContainer("rabbitmq:3.12-management-alpine")
+                .withAdminUser("admin")
+                .withAdminPassword("password");
+    }
+
+}
+
