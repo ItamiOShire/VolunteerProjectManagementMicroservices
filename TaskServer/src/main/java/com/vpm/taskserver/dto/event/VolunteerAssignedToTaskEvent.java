1201a6b5a90b463aacba8250ddcff97494f4e7b7
Adam <a.niewczas01@gmail.com>
2026-05-06 13:08:23 +0200

    fix-taskserver: dto implements Serializable interface to be convertable by rabbit template

--- TaskServer/src/main/java/com/vpm/taskserver/dto/event/VolunteerAssignedToTaskEvent.java
+++ TaskServer/src/main/java/com/vpm/taskserver/dto/event/VolunteerAssignedToTaskEvent.java
@@ -3,11 +3,12 @@ package com.vpm.taskserver.dto.event;
 import lombok.Builder;
 import lombok.Data;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 
 @Data
 @Builder
-public class VolunteerAssignedToTaskEvent {
+public class VolunteerAssignedToTaskEvent implements Serializable {
 
     private Long volunteerId;
     private Long taskId;
