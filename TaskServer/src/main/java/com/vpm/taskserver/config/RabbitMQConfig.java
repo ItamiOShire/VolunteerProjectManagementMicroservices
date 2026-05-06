ecea7781f1213388ed0d8cfa43b225327c09b846
Adam <a.niewczas01@gmail.com>
2026-05-06 13:07:32 +0200

    fix-taskserver: set message converter for rabbit template

--- TaskServer/src/main/java/com/vpm/taskserver/config/RabbitMQConfig.java
+++ TaskServer/src/main/java/com/vpm/taskserver/config/RabbitMQConfig.java
@@ -5,6 +5,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -135,6 +137,7 @@ public class RabbitMQConfig {
             ConnectionFactory connectionFactory
     ) {
         RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(jsonMessageConverter());
         template.setMandatory(true);
 
         template.setConfirmCallback(
@@ -156,6 +159,11 @@ public class RabbitMQConfig {
         );
 
         return template;
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
     }
 
 }
