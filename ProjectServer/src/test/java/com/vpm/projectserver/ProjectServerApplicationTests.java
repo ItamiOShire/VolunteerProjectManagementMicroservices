ad6314aeee44dc9cf54f6aae0e4d6927396e7c80
Adam <a.niewczas01@gmail.com>
2026-05-08 14:46:52 +0200

    fix-projectserver: build does not rely on real db

--- ProjectServer/src/test/java/com/vpm/projectserver/ProjectServerApplicationTests.java
+++ ProjectServer/src/test/java/com/vpm/projectserver/ProjectServerApplicationTests.java
@@ -1,9 +1,15 @@
 package com.vpm.projectserver;
 
+import com.vpm.projectserver.config.IntegrationTestsDBConfig;
+import com.vpm.projectserver.config.IntegrationTestsRabbitMQConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
+@Import({
+        IntegrationTestsDBConfig.class,
+        IntegrationTestsRabbitMQConfig.class})
 class ProjectServerApplicationTests {
 
     @Test
